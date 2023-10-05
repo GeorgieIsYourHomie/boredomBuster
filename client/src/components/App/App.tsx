3a0d37e1786331a3256993eeac48491602a645ff
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-10-04 19:00:28 -0500

    Created newreact typescript project for this directory. Removed unecessary react code. Created entire folder sturcture for tis project. At root, a tsconfig, package.json and readme file.Also added a dist folder for all client and server compiled js code, each with their own respective folder within. | Created a client folder that is holding public, src, components, with subsequent files, as well as index, package.json and tsconfig file for client. | In server folder, using src, controllers, and routes foldes , along with server.ts and tsconfig and package.json files. | In shared folder, created constants, enums, sharedModals, and utils folders for shared code not exclusive to either client or server.

---
+++ client/src/components/App/App.tsx
@@ -0,0 +1,17 @@
+// * - IMPORTING -
+import React from "react";
+import "./App.css";
+
+// * - App COMPONENT -
+const App: React.FC = () => {
+  return (
+    <React.Fragment>
+      <header>
+        <h1>Boredom Buster</h1>
+      </header>
+    </React.Fragment>
+  );
+};
+
+// * Exporting App Component
+export default App;
