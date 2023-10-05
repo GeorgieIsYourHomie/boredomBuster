7d9ffe0fd402c634744dcc0cec4cc1d5bb66e56e
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-10-04 23:09:41 -0500

    Created tsconfig file for root directory to compile client and server code to /dist folder in seperate folders. Added tsconfig files for client and server as well, extending to that root folder. Created index and server TypeScript files in respective folders once more for transpilation.  | Creaded code for App for transpile. | Deleted unecessary folders like alternative named folder for config file and extra index.

---
+++ dist/server/src/server.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Server = void 0;
+const jsx_runtime_1 = require("react/jsx-runtime");
+// * - IMPORTING -
+const react_1 = require("react");
+const Server = () => {
+    return ((0, jsx_runtime_1.jsx)(react_1.default.Fragment, { children: (0, jsx_runtime_1.jsx)("h1", { children: "Server" }) }));
+};
+exports.Server = Server;
---
+++ server/src/server.tsx
@@ -0,0 +1,10 @@
+// * - IMPORTING -
+import React from "react";
+
+export const Server = () => {
+  return (
+    <React.Fragment>
+      <h1>Server</h1>
+    </React.Fragment>
+  );
+};
