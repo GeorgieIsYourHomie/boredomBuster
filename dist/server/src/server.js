e12e7b9efebbaa8b1629f34792542b666f29c54b
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-10-07 00:17:29 -0500

    Removed eslint config file. Added static middleware to load public. THis isnt working currently for some reason. Will figure it out. May have somwthing to do with ts compiling and index root referencing a typescript file.

--- dist/server/src/server.js
+++ dist/server/src/server.js
@@ -18,9 +18,10 @@ const port = process.env.PORT || 5000;
 // * - MIDDLEWARE -
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(body_parser_1.default.json());
-// Serve static files from the "public" directory inside the "client" directory
-app.use(express_1.default.static(path_1.default.join(__dirname, "client", "public")));
+// Serve static files from the "public" directory (which should contain your React app build files)
+app.use(express_1.default.static(path_1.default.join(__dirname, "client", "public/index.html")));
 // * - ROUTES -
+// Bored API
 app.use("/api/activity", activities_router_1.default);
 // * - START SERVER -
 app.listen(port, () => {
--- server/src/server.ts
+++ server/src/server.ts
@@ -14,10 +14,11 @@ const port = process.env.PORT || 5000;
 // * - MIDDLEWARE -
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// Serve static files from the "public" directory inside the "client" directory
-app.use(express.static(path.join(__dirname, "client", "public")));
+// Serve static files from the "public" directory (which should contain your React app build files)
+app.use(express.static(path.join(__dirname, "client", "public/index.html")));
 
 // * - ROUTES -
+// Bored API
 app.use("/api/activity", activityRouter);
 
 // * - START SERVER -
