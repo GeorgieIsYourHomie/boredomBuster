b9448ac06de348a7bd4aa6f4cbf56d8b2f903dde
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-10-06 22:25:25 -0500

    In server.ts updated code to import packages with import syntax rather than require. Added new url for accessing activityRouter. Added Path node package to help woth redirecting server to folder path of public that is in client folder. | Created activity router file to make get requests to the bored API. Getting data from the random route of the API. | Created files for Controllers and Models folders but no code in Contollers folder  yet. | In Models folder, added activity.ts file that will be used for an interface type for the bored data from API.

--- server/dist/server/controllers/activity.controller.js
+++ server/dist/server/controllers/activity.controller.js
@@ -1,13 +1,1 @@
 "use strict";
-// // * - IMPORTING -
-// // Express
-// import { Request, Response } from "express";
-// // Activity Model
-// import { Activity } from "../../../shared/models/activity";
-// const activities: Activity[] = []; // You can store activities in memory or use a database
-// // Get a random activity
-// export const getRandomActivity = (req: Request, res: Response) => {
-//   const randomIndex = Math.floor(Math.random() * activities.length);
-//   const randomActivity = activities[randomIndex];
-//   res.json(randomActivity);
-// };
