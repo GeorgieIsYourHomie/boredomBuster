d670df1028c57a0046faef25baa87251a82046d9
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-10-06 19:01:37 -0500

    Added an .eslintrs.json file tha added options for client folder overrides. | In root package.json file, updated scipts for server and client side. | Removed default react css for index and app. | Configured server.ts file to run a server with express. | Compiler working properly. | Created boredRouter for server router at url /bored.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // * - IMPORTING AND DECLARING -
 require("dotenv").config();
 const express = require("express");
@@ -7,13 +5,18 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 const boredRouter = require("./routes/bored.router");
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static("build"));
+
 /** ---------- ROUTES ---------- **/
 app.use("/bored", boredRouter);
+
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
-    console.log("Listening on port: ", port);
+  console.log("Listening on port: ", port);
 });
+
+export {};
