fc8984c901e21f1d165f1d4bea3c6b00a6617a2b
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-10-16 16:53:57 -0500

    Added hover animation style to find activity button.

--- client/src/components/FindActivity/FindActivityButton/FindActivityButton.tsx
+++ client/src/components/FindActivity/FindActivityButton/FindActivityButton.tsx
@@ -7,7 +7,7 @@ const FindActivityButton: () => JSX.Element = () => {
   return (
     <React.Fragment>
       <div className="mb-2">
-        <button className="m-4 mt-10 transform rounded-full bg-indigo-700 px-10 py-2 text-3xl text-indigo-100 shadow-[0px_9px_30px_-10px_rgba(74,63,205)] transition-transform duration-150 hover:bg-white hover:text-black hover:shadow-[0px_9px_30px_-4px_rgba(74,63,205)]  active:scale-75">
+        <button className="hover:floating m-4 mt-10 transform rounded-full bg-indigo-700 px-10 py-2 text-3xl text-indigo-100 shadow-[0px_9px_30px_-10px_rgba(74,63,205)] transition-transform duration-150 hover:scale-110 hover:bg-white hover:text-black hover:shadow-[0px_9px_30px_-4px_rgba(74,63,205)] active:scale-75">
           Find Activity
         </button>
       </div>
