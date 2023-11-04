6b9ed1072c5c0209be8a270ae8c59ff489bad36b
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-11-04 12:53:27 -0500

    Created an error reducer for creating errorPrompt for the app. | Created FindActivityError component that will hold the error prompt for rendering. | Calling FindActivityError component into FindActivityComponent. | FindActivityButton sending out dispatch to clear errors is there is a chosen category or trigger error reducer is there is no chosenCategory upon button click.

---
+++ client/src/components/FindActivity/FindActivityButton/Error/FindActivityError.tsx
@@ -0,0 +1,17 @@
+// * - IMPORTING -
+// React
+import React from "react";
+// Hooks
+import { useAppSelector } from "../../../../hooks/reduxHook/reduxHook";
+
+// * - FindActivityError COMPONENT -
+const FindActivityError: React.FC = () => {
+  // * - DECLARATIONS -
+  const errorPrompt = useAppSelector((store) => store.errorReducer.errorPrompt);
+
+  // * - RENDERING -
+  return <React.Fragment>{errorPrompt && <p className="mt-8 text-lg text-rose-600">{errorPrompt}</p>}</React.Fragment>;
+};
+
+// * Exporting FindActivityError
+export default FindActivityError;
--- client/src/components/FindActivity/FindActivityButton/FindActivityButton.tsx
+++ client/src/components/FindActivity/FindActivityButton/FindActivityButton.tsx
@@ -7,23 +7,42 @@ import { useAppDispatch } from "../../../hooks/reduxHook/reduxHook";
 import type { ChosenCategories } from "../../../types/categories/CategoryTypes";
 
 // * - FindActivityButton COMPONENT -
-const FindActivityButton: React.FC<{ chosenCategories: ChosenCategories }> = ({
-  chosenCategories,
-}) => {
+const FindActivityButton: React.FC<{
+  chosenCategories: ChosenCategories;
+}> = ({ chosenCategories }) => {
   // * - DECLARATIONS -
   const dispatch = useAppDispatch();
 
   // * - FUNCTIONS -
   // * Function to loop through chosenCategories
   //  If value matches any category, send dispatch
-  const handleFindActivityButton = () => {
+  const handleFindActivityButton = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+
     console.log("chosenCategories are:", chosenCategories);
 
-    // Dispatching action and payload of chosenCategories for activities
-    dispatch({
-      type: "SEARCH_CHOSEN_CATEGORIES_ACTIVITY",
-      payload: chosenCategories,
-    });
+    // chosenCategories true, dispatch action,
+    // otherwise toggle on prompt
+    if (chosenCategories && chosenCategories.length > 0) {
+      // Dispatching action and payload of chosenCategories for activities
+      dispatch({
+        type: "SEARCH_CHOSEN_CATEGORIES_ACTIVITY",
+        payload: chosenCategories,
+      });
+      // Dispatching action to clear errors
+      dispatch({
+        type: "NO_ERRORS",
+      });
+    } else if (chosenCategories && chosenCategories?.length === 0) {
+      // Error prompt dispatch here if no selected category
+      dispatch({
+        type: "SELECT_A_CATEGORY",
+      });
+
+      console.log("Please select a category.");
+    } else {
+      return;
+    }
   }; // * end handleFindActivityButton
 
   // * - RENDERING -
@@ -32,7 +51,7 @@ const FindActivityButton: React.FC<{ chosenCategories: ChosenCategories }> = ({
       <div className="mb-2">
         <button
           onClick={handleFindActivityButton}
-          className=" m-6 mb-8 mt-12 transform rounded-full bg-indigo-700 px-10 py-2 text-2xl text-white shadow-[0px_9px_30px_-10px_rgba(74,63,205)] transition-transform duration-150 hover:scale-110 hover:border hover:border-black hover:bg-white hover:text-black hover:shadow-[0px_9px_30px_-4px_rgba(74,63,205)] active:scale-75"
+          className="mb-8 mt-14 transform rounded-full bg-indigo-700 px-10 py-2 text-2xl text-white shadow-[0px_9px_30px_-10px_rgba(74,63,205)] transition-transform duration-150 hover:scale-110 hover:border hover:border-black hover:bg-white hover:text-black hover:shadow-[0px_9px_30px_-4px_rgba(74,63,205)] active:scale-75"
         >
           Find Activity
         </button>
