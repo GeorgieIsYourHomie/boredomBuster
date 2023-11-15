62e8e6e48206654549acaaa368e9105d23d107b1
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-11-14 22:49:05 -0600

    Ran build.

--- client/build/src/client/src/components/FindActivity/FindActivity.js
+++ client/build/src/client/src/components/FindActivity/FindActivity.js
@@ -20,7 +20,7 @@ const FindActivity = ({ selectedCategories, chosenCategories, toggleCategory, })
     // toggleShowActivity reducer
     const toggleShowActivity = (0, reduxHook_1.useAppSelector)((store) => store.toggleShowActivityReducer.toggleShowActivity);
     // * - RENDERING -
-    return ((0, jsx_runtime_1.jsx)(react_1.default.Fragment, { children: (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "max-w-full rounded-2xl border-2 border-black bg-white p-7 shadow-[24px_32px_0px_2px_rgba(0,0,0,90)]" }, { children: [(0, jsx_runtime_1.jsx)(Categories_1.default, { selectedCategories: selectedCategories, chosenCategories: chosenCategories, toggleCategory: toggleCategory }), (0, jsx_runtime_1.jsx)(FindActivityError_1.default, {}), (0, jsx_runtime_1.jsx)(FindActivityButton_1.default, { chosenCategories: chosenCategories }), toggleShowActivity && chosenCategories && chosenCategories.length > 0 && (0, jsx_runtime_1.jsx)(ShowActivity_1.default, {})] })) }));
+    return ((0, jsx_runtime_1.jsx)(react_1.default.Fragment, { children: (0, jsx_runtime_1.jsxs)("div", Object.assign({ className: "max-w-full rounded-2xl border-4 border-black bg-white p-7 shadow-[24px_32px_0px_2px_rgba(0,0,0,90)]" }, { children: [(0, jsx_runtime_1.jsx)(Categories_1.default, { selectedCategories: selectedCategories, chosenCategories: chosenCategories, toggleCategory: toggleCategory }), (0, jsx_runtime_1.jsx)(FindActivityError_1.default, {}), (0, jsx_runtime_1.jsx)(FindActivityButton_1.default, { chosenCategories: chosenCategories }), toggleShowActivity && chosenCategories && chosenCategories.length > 0 && (0, jsx_runtime_1.jsx)(ShowActivity_1.default, {})] })) }));
 }; // * - END FindActivityButton COMPONENT -
 // * Exporting FindActivityButton Component
 exports.default = FindActivity;
